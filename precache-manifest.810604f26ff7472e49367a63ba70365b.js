c8c53f07494f00c558ddbcff8d2372e6d1430225
Ivan Lei <ilei0893@gmail.com>
2020-05-28 19:42:26 -0400

    Updates

---
+++ precache-manifest.810604f26ff7472e49367a63ba70365b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18ef01cee56ebb2548ad6326010a181",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "a0a33313feece1c5cbc9",
+    "url": "/Covid19Tracker/static/css/main.fd0c0a12.chunk.css"
+  },
+  {
+    "revision": "031f120a4b55c69094eb",
+    "url": "/Covid19Tracker/static/js/2.8d842873.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker/static/js/2.8d842873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a33313feece1c5cbc9",
+    "url": "/Covid19Tracker/static/js/main.61c2fd98.chunk.js"
+  },
+  {
+    "revision": "52c3f5ff242a45da05e3",
+    "url": "/Covid19Tracker/static/js/runtime-main.407e27ba.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19Tracker/static/media/image.d7265326.png"
+  }
+]);
